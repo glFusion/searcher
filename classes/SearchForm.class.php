8c0d93ca5299bafe2a130570985d8e60a2d2bfbf
Lee Garner <lee@leegarner.com>
2017-07-21 14:56:06 -0700

    Standardize on query as parameter name

--- classes/SearchForm.class.php
+++ classes/SearchForm.class.php
@@ -33,10 +33,10 @@ class SearchForm extends Common
         global $_CONF, $_TABLES;
 
         // Set search criteria
-        if (isset($_GET['q'])) {
-            $this->_query = strip_tags($_GET['q']);
-        } else if (isset($_POST['q'])) {
-            $this->_query = strip_tags($_POST['q']);
+        if (isset($_GET['query'])) {
+            $this->_query = strip_tags($_GET['query']);
+        } else if (isset($_POST['query'])) {
+            $this->_query = strip_tags($_POST['query']);
         } else {
             $this->_query = '';
         }
@@ -219,142 +219,13 @@ class SearchForm extends Common
         $T = new \Template(SRCH_PI_PATH . '/templates');
         $T->set_file(array('searchform' => 'searchform.thtml'));
         $T->set_var(array(
-            /*'search_intro'  => $LANG09[19],
-            'lang_keywords' => $LANG09[2],
-            'lang_date'     => $LANG09[20],
-            'lang_to'       => $LANG09[21],
-            'date_format'   => $LANG09[22],
-            'lang_topic'    => $LANG09[3],
-            'lang_all'      => $LANG09[4],
-            'topic_option_list' => COM_topicList ('tid,topic', $this->_topic),
-            'lang_type'     => $LANG09[5],
-            'lang_results'  => $LANG09[59],
-            'lang_per_page' => $LANG09[60],
-            'lang_exact_phrase' => $LANG09[43],
-            'lang_all_words'    => $LANG09[44],
-            'lang_any_word'     => $LANG09[45],*/
             'query'         => htmlspecialchars($this->_query),
-            /*'datestart'     => $this->_dateStart,
-            'dateend'       => $this->_dateEnd,
-            'key_' . $this->_keyType . '_selected' => 'selected="selected"',*/
         ) );
-/*
-        $options = '';
-        if ( isset($_CONF['comment_engine']) && $_CONF['comment_engine'] != 'internal') {
-            $plugintypes = array('all' => $LANG09[4], 'stories' => $LANG09[6]);
-        } else {
-            $plugintypes = array('all' => $LANG09[4], 'stories' => $LANG09[6], 'comments' => $LANG09[7]);
-        }
-        $plugintypes = array_merge($plugintypes, PLG_getSearchTypes());
-
-        foreach ($plugintypes as $key => $val) {
-            $options .= "<option value=\"$key\"";
-            if ($this->_type == $key)
-                $options .= ' selected="selected"';
-            $options .= ">$val</option>".LB;
-        }
-        $plugin_types_option = $options;
-        $T->set_var('plugin_types', $options);
-
-        if ($_CONF['contributedbyline'] == 1) {
-            $T->set_var('lang_authors', $LANG09[8]);
-            $searchusers = array();
-            if ( $_CONF['comment_engine'] == 'internal' && isset($_TABLES['comments'])) {
-                $result = DB_query("SELECT DISTINCT uid FROM {$_TABLES['comments']}");
-                while ($A = DB_fetchArray($result)) {
-                    $searchusers[$A['uid']] = $A['uid'];
-                }
-            }
-            if ( isset($_TABLES['stories'])) {
-                $result = DB_query("SELECT DISTINCT uid FROM {$_TABLES['stories']} WHERE (date <= NOW()) AND (draft_flag = 0)");
-                while ($A = DB_fetchArray($result)) {
-                    $searchusers[$A['uid']] = $A['uid'];
-                }
-            }
-
-            if (in_array('forum', $_PLUGINS) && isset($_TABLES['ff_topic'] ) ) {
-                $result = DB_query("SELECT DISTINCT uid FROM {$_TABLES['ff_topic']}");
-                while ( $A = DB_fetchArray($result)) {
-                    $searchusers[$A['uid']] = $A['uid'];
-                }
-            }
-
-            $inlist = implode(',', $searchusers);
-
-            if (!empty ($inlist)) {
-                $sql = "SELECT uid,username,fullname FROM {$_TABLES['users']} WHERE uid IN ($inlist)";
-                if (isset ($_CONF['show_fullname']) && ($_CONF['show_fullname'] == 1)) {
-*/
-                    /* Caveat: This will group all users with an emtpy fullname
-                     *         together, so it's not exactly sorted by their
-                     *         full name ...
-                     */
-/*                    $sql .= ' ORDER BY fullname,username';
-                } else {
-                    $sql .= ' ORDER BY username';
-                }
-                $result = DB_query ($sql);
-                $options = '';
-                $options .= '<option value="all">'.$LANG09[4].'</option>'.LB;
-                while ($A = DB_fetchArray($result)) {
-                    $options .= '<option value="' . $A['uid'] . '"';
-                    if ($A['uid'] == $this->_author) {
-                        $options .= ' selected="selected"';
-                    }
-                    $options .= '>' . htmlspecialchars(COM_getDisplayName($A['uid'], $A['username'], $A['fullname'])) . '</option>';
-                }
-                $T->set_var('author_option_list', $options);
-                $T->parse('author_form_element', 'authors', true);
-            } else {
-                $T->set_var('author_form_element', '<input type="hidden" name="author" value="0"' . XHTML . '>');
-            }
-        } else {
-            $T->set_var ('author_form_element',
-                    '<input type="hidden" name="author" value="0"' . XHTML . '>');
-        }
-
-        $searchTimeOptions = array(
-            '0' => $LANG09[4],
-            '1' => $LANG09[75],
-            '7' => $LANG09[76],
-            '14' => $LANG09[77],
-            '30' => $LANG09[78],
-            '90' => $LANG09[79],
-            '180' => $LANG09[80],
-            '365' => $LANG09[81]);
-
-        // search time frame
-        $options = '';
-        foreach ( $searchTimeOptions AS $days => $prompt ) {
-            $options .= '<option value="'.$days.'"';
-            if ( $this->_searchDays == $days ) {
-                $options .= ' selected="selected"';
-            }
-            $options .= '>'.$prompt.'</option>'.LB;
-        }
-        $date_option = $options;
-        $T->set_var('search_time',$options);
-
-        // Results per page
-        $options = '';
-        $limits = explode(',', $_CONF['search_limits']);
-        foreach ($limits as $limit) {
-            $options .= "<option value=\"$limit\"";
-            if ($this->_results == $limit) {
-                $options .= ' selected="selected"';
-            }
-            $options .= ">$limit</option>" . LB;
-        }
-        $search_limit_option = $options;
-        $T->set_var('search_limits', $options);
-*/
-//        $T->set_var('lang_search', $LANG09[10]);
         $T->parse('output', 'searchform');
-
         $retval .= $T->finish($T->get_var('output'));
-
         return $retval;
     }
+
 
     private function _validateDate( $dateString )
     {
@@ -375,6 +246,7 @@ class SearchForm extends Common
         }
         return true;
     }
+
 }
 
 ?>
