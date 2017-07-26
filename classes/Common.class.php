ac31691a2a4e3036fe1d17a0698e15580db613ff
Lee Garner <lee@leegarner.com>
2017-07-26 14:11:50 -0700

    Remove extra quote in str_replace pattern

--- classes/Common.class.php
+++ classes/Common.class.php
@@ -81,7 +81,7 @@ class Common
             chr(194) . chr(160),
             "'", '&nbsp;', '&#8217;', '"',
             "\n", "\t", '(', ')', '{', '}', '%', '$', '#', '[', ']', '+', '=',
-            '_', '-', '"', '`', ',', '<', '>', '=', ':', '?', ';', '&', '/', '\\',
+            '_', '-', '`', ',', '<', '>', '=', ':', '?', ';', '&', '/', '\\',
         );
 
         //$str = preg_replace ('/<[^>]*>/', ' ', $str);
@@ -178,6 +178,19 @@ class Common
                 $i--;
             }
             $weights[$i] = 1;
+        }
+
+        // Invoke the stemmer to trim words down to their stems.
+        if (!empty($_SRCH_CONF['stemmer'])) {
+            USES_search_class_stemmer();
+            $S = Stemmer::getInstance($_SRCH_CONF['stemmer']);
+            if ($S !== NULL) {
+                for ($i = 0; $i < count($terms); $i++) {
+                    $terms[$i] = $S->stem($terms[$i]);
+                }
+                // Now remove any duplicates (words with the same stem)
+                $terms = array_keys(array_flip($terms)); 
+            }
         }
 
         // Now go through the terms array and add 2- and 3-word phrases
