e808438b722fc43e9f689b85c8c38427bb0ba8fe
Mark R. Evans <mark@glfusion.org>
2017-07-31 15:29:55 -0500

    Initial ajax reindexer implementation

---
+++ admin/indexer.js
@@ -0,0 +1,308 @@
+/**
+ * glFusion Searcher Plugin - Ajax Indexer
+ *
+ * Indexes all content types
+ *
+ * LICENSE: This program is free software; you can redistribute it
+ *  and/or modify it under the terms of the GNU General Public License
+ * as published by the Free Software Foundation; either version 2
+ * of the License, or (at your option) any later version.
+ *
+ * @category   glFusion CMS
+ * @package    searcher
+ * @author     Mark R. Evans  mark AT glFusion DOT org
+ * @copyright  2015-2017 - Mark R. Evans
+ * @license    http://opensource.org/licenses/gpl-2.0.php - GNU Public License v2 or later
+ *
+ */
+
+/* ***************************************************************************
+ * NOTE: Must update indexer.min.js for distribution
+ * ***************************************************************************
+ */
+
+
+ // flow
+ // - searcherinit
+ //   - pub.update
+ //   - processContentTypes (get list of content types)
+ //   - initContent (get list of content items)
+ //     -- processContent (index each specific item)
+ //   - initContent
+
+var searcherinit = (function() {
+
+    // public methods/properties
+    var pub = {};
+
+    // private vars
+    var contenttypes  = null,  // array of all content types
+    contenttype       = null,  // current table being processed
+    contentLists      = null,
+    contentCount = 0,
+    contentDone  = 1,
+    url         = null,
+    done        = 1,
+    count       = 0,
+    contenttypeErrorCount  = 0,  // track errors per table - reset each time we start a new table - if non-zero do not convert table
+    indexingMessage = '',
+    $m          = null;
+    $t          = null;
+
+    /**
+    * sets HTML
+    * retrieves content type list from glFusion
+    *
+    * calls indexContentType
+    *
+    */
+    pub.update = function() {
+        done = 1;
+
+        console.log('Indexer: In pub.update - retrieving list of content types');
+
+        // retrieve the URL from the form
+        url = $( '#reindexform' ).attr( 'action' );
+
+        $("#indexer_batchprocesor").show();
+        $('#reindexbutton').prop("disabled",true);
+        $("#reindexbutton").html(lang_indexing);
+        t_on();
+
+        message(lang_retrieve_content_types);
+
+        $.ajax({
+            type: "POST",
+            dataType: "json",
+            url: url,
+            data: {"getcontenttypes" : "x" },
+            timeout: 30000,
+        }).done(function(data) {
+            var result = $.parseJSON(data["js"]);
+            if ( result.errorCode != 0 ) {
+                console.log('Indexer: Content Type List returned no results');
+                alert(lang_error_getcontenttypes);
+                window.location.href = destination;
+            }
+            contenttypes = result.contenttypes;
+            count = contenttypes.length;
+            contenttype = contenttypes.shift();
+            window.setTimeout(processContentTypes,250);
+        }).fail(function(jqXHR, textStatus ) {
+            console.log("Indexer: Error retrieving content type list from glFusion");
+            alert(lang_error_getcontenttypes);
+            window.location.href = destination;
+        });
+        return false; // prevent from firing
+    };
+
+    /**
+    * initialize everything
+    */
+    pub.init = function() {
+        if ( gl != 'log') return;
+        // $m is the status message area
+        $m = $('#batchinterface_msg');
+        $t = $('#t');
+        // if $m does not exist, return.
+        if ( ! $m) {
+            return;
+        }
+        console.log('Indexer: In pub.init - calling pub.update');
+        // init interface events
+        $('#reindexbutton').click(pub.update);
+    };
+
+    /**
+    * Processes all content for content type
+    *
+    * controls UI updates
+    *
+    * calls init colums
+    */
+
+    var processContentTypes = function() {
+        console.log('Indexer: In processContentTypes()');
+        if (contenttype) {
+            console.log('Indexer: In processContentTypes() - processing type: ' + contenttype);
+            var percent = Math.round(( ( done - 1 ) / count ) * 100);
+            if ( percent == 0 ) {
+                percent = Math.round((( done  / count ) * 100) / 2 );
+            }
+            $('#pb').css('width', percent + "%");
+            $('#pb').html(percent + "%");
+            contenttypeErrorCount = 0;
+            var wait = 250;
+            window.setTimeout(initContent, wait);
+        } else {
+            console.log('Indexer: In processContentTypes() - reindexing content');
+            finished();
+        }
+    };
+
+    /**
+    * retrieves a list of all content types (item ids)
+    */
+
+    var initContent = function() {
+        if (contenttype) {
+            console.log("processing content type " + contenttype);
+            itemList = null;
+
+            var dataS = {
+                "getcontentlist" : 'x',
+                "type" : contenttype,
+            };
+
+            data = $.param(dataS);
+
+            contentDone = 1;
+            var percent = 0;
+            $('#pb-current').css('width', percent + "%");
+            $('#pb-current').html(percent + "%");
+
+            $.ajax({
+                type: "POST",
+                dataType: "json",
+                url: url,
+                data: data,
+                timeout:60000
+            }).done(function(data) {
+                var result = $.parseJSON(data["js"]);
+                if ( result.errorCode != 0 ) {
+                    console.log("Indexer: " + result.message );
+                    contentypeErrorCount++;
+                    indexingMessage = indexingMessage + 'ContentType: ' + contenttype + ' :: ' + result.message + '<br>';
+                }
+                contentLists = result.contentlist;
+                contentCount = contentLists.length;
+                console.log("There are " + contentCount + " items for content type " + contenttype);
+                content = contentLists.shift();
+            }).fail(function(jqXHR, textStatus ) {
+                indexingMessage = indexingMessage + 'ContentType: ' + contenttype + ' :: ' + 'Failed to retrieve content list<br>';
+                if (textStatus === 'timeout') {
+                    console.log("Indexer: Unable to retrieve content list for " + contenttype);
+                    alert(lang_error_getcontentlist + " :: " + contenttype);
+                    window.location.href = destination;
+                }
+            }).always(function( xhr, status ) {
+                var wait = 250;
+                window.setTimeout(processContent, wait);
+            });
+        }
+    };
+
+    /**
+    * process each content type for content type
+    *
+    * if a column cannot convert - display in the error section
+    */
+
+    var processContent = function() {
+
+        if (content) {
+
+            var dataS = {
+                "index" : 'x',  // need to change.
+                "type" : contenttype,
+                "id" : content.id,
+            };
+
+            data = $.param(dataS);
+
+            message(lang_indexing + ' ' + done + '/' + count + ' - '+ contenttype + ' - ' + content.id);
+
+            var percent = Math.round(( contentDone / contentCount ) * 100);
+
+            if ( percent == 0 ) percent = 1;
+
+            $('#pb-current').css('width', percent + "%");
+            $('#pb-current').html(percent + "%");
+
+            $.ajax({
+                type: "POST",
+                dataType: "json",
+                url: url,
+                data: data,
+                timeout:60000
+            }).done(function(data) {
+                var result = $.parseJSON(data["js"]);
+                if ( result.errorCode != 0 ) {
+                    console.log("Indexer: Error indexing content");
+                    contenttypeErrorCount++;
+                    indexingMessage = indexingMessage + 'ContentType: ' + contenttype + ' ID: ' + content.id + ' :: ' + result.statusMessage + '<br>';
+                }
+            }).fail(function(jqXHR, textStatus ) {
+                if (textStatus === 'timeout') {
+                    console.log("Indexer: Error indexing item " + content);
+                    indexingMessage = indexingMessage + 'Content Type: ' + contenttype + ' ID: ' + content.id + ' :: ' + result.statusMessage + '<br>';
+                }
+            }).always(function( xhr, status ) {
+                contentDone++;
+                content = contentLists.shift();
+                var wait = 50;
+                window.setTimeout(processContent, wait);
+            });
+        } else {
+            var percent = 100;
+            done++;
+            $('#pb-current').css('width', percent + "%");
+            $('#pb-current').html(percent + "%");
+            contenttype = contenttypes.shift();
+            window.setTimeout(processContentTypes,250);
+        }
+    };
+
+    /**
+    * called when done - we make an ajax call
+    * that currently fails as we are not setup for this one
+    * need to update.
+    */
+
+    var finished = function() {
+
+        $('#pb').css('width', "100%");
+        $('#pb').html("100%");
+        t_off();
+        message(lang_success);
+
+        window.setTimeout(function() {
+            UIkit.modal.alert(lang_success);
+            var $msgWindow = $('#indexer_messages');
+            if ( indexingMessage == '' ) indexingMessage = lang_no_errors;
+            $msgWindow.html(indexingMessage);
+            $("#indexer_message_window").show();
+            $("#reindexbutton").html(lang_index);
+            $('#reindexbutton').prop("disabled",false);
+        }, 3000);
+    };
+
+    /**
+    * Gives textual feedback
+    * updates the ID defined in the $msg variable
+    */
+    var message = function(text) {
+        $m.html(text);
+    };
+
+    /**
+    * add a throbber image
+    */
+    var t_on = function() {
+        $t.show();
+    };
+
+    /**
+    * Stop the throbber
+    */
+    var t_off = function() {
+        $t.hide();
+    };
+
+    // return only public methods/properties
+    return pub;
+})();
+
+$(function() {
+    searcherinit.init();
+});
---
+++ admin/reindex.php
@@ -0,0 +1,276 @@
+<?php
+/**
+*   Searcher Plugin for glFusion CMS - reindex site content
+*
+*   @author     Mark R. Evans <mark@glfusion.org>
+*   @copyright  Copyright (c) 2017 Mark R. Evans <mark AT glFusion DOT org>
+*   @package    searcher
+*   @version    0.0.3
+*   @license    http://opensource.org/licenses/gpl-2.0.php
+*               GNU Public License v2 or later
+*   @filesource
+*/
+require_once '../../../lib-common.php';
+require_once '../../auth.inc.php';
+
+// temp lang strings - will move to language file when done...
+
+$LANG_SRCH_ADM = array(
+    'reindex_title'     => 'Reindex Content',
+    'searcher_admin'    => 'Searcher Admin',
+    'index_instructions'    => 'This will scan all content types and rebuild the searcher index',
+    'reindex_button'    => 'Reindex',
+    'success'           => 'Success',
+    'indexing'          => 'Indexing',
+    'index_status'      => 'Indexing Status',
+    'retrieve_content_types'   => 'Retrieving Content Types',
+    'error_heading'     => 'Errors',
+    'no_errors'         => 'No Errors',
+    'error_getcontenttypes' => 'Unable to retrieve content types from glFusion',
+    'current_progress'  => 'Current Progress',
+    'overall_progress'  => 'Overall Progress',
+
+);
+
+USES_lib_admin();
+
+function SRCHER_reindex()
+{
+    global $_CONF, $_SRCH_CONF, $LANG01, $LANG_ADMIN, $LANG_SRCH_ADM, $_IMAGE_TYPE;
+
+    $retval = '';
+
+    $T = new Template(SRCH_PI_PATH . '/templates');
+    $T->set_file('page','reindex.thtml');
+
+    $menu_arr = SRCH_getAdminHeaderMenu('reindex');
+
+    $T->set_var('start_block', COM_startBlock($LANG_SRCH_ADM['searcher_admin'], '',
+                        COM_getBlockTemplate('_admin_block', 'header')));
+
+    $T->set_var('admin_menu',ADMIN_createMenu(
+                $menu_arr,
+                "",
+                plugin_geticon_searcher())
+    );
+
+    $T->set_var('lang_title',$LANG_SRCH_ADM['reindex_title']);
+
+    $T->set_var('lang_conversion_instructions', $LANG_SRCH_ADM['index_instructions']);
+
+    $T->set_var('security_token',SEC_createToken());
+    $T->set_var('security_token_name',CSRF_TOKEN);
+    $T->set_var(array(
+        'form_action'       => $_CONF['site_admin_url'].'/plugins/searcher/reindex.php',
+        'lang_index'        => $LANG_SRCH_ADM['reindex_button'],
+        'lang_cancel'       => $LANG_ADMIN['cancel'],
+        'lang_ok'           => $LANG01['ok'],
+        'lang_indexing'     => $LANG_SRCH_ADM['indexing'],
+        'lang_success'      => $LANG_SRCH_ADM['success'],
+        'lang_ajax_status'  => $LANG_SRCH_ADM['index_status'],
+        'lang_retrieve_content_types' => $LANG_SRCH_ADM['retrieve_content_types'],
+        'lang_error_header' => $LANG_SRCH_ADM['error_heading'],
+        'lang_no_errors'    => $LANG_SRCH_ADM['no_errors'],
+        'lang_error_getcontenttypes' => $LANG_SRCH_ADM['error_getcontenttypes'],
+
+        'lang_current_progress' => $LANG_SRCH_ADM['current_progress'],
+        'lang_overall_progress' => $LANG_SRCH_ADM['overall_progress'],
+    ));
+    $T->set_var('end_block',COM_endBlock(COM_getBlockTemplate('_admin_block', 'footer')));
+
+    $T->parse('output', 'page');
+    $retval .= $T->finish($T->get_var('output'));
+
+    return $retval;
+}
+
+/*
+ * Build admin menu
+ */
+function SRCH_getAdminHeaderMenu( $activeItem = '' )
+{
+    global $_CONF, $LANG_ADMIN, $LANG_SRCH_ADM;
+
+    $menu_items = array(
+        'searcher_admin' => array(
+                'url'  => $_CONF['site_admin_url'].'/plugins/searcher/index.php',
+                'text' => $LANG_SRCH_ADM['searcher_admin']
+                ),
+        'reindex' => array(
+                'url'   => $_CONF['site_admin_url'].'/plugins/searcher/reindex.php',
+                'text'  => $LANG_SRCH_ADM['reindex_title']
+                ),
+        'admin_home'    => array(
+                'url' => $_CONF['site_admin_url'],
+                'text' => $LANG_ADMIN['admin_home']
+                )
+    );
+    $menu_arr = array();
+    foreach ($menu_items AS $item => $info ) {
+        $active = 0;
+        if ( $activeItem == $item ) $active = 1;
+        $menu_arr[] = array(
+                        'url' => $info['url'],
+                        'text'=> $info['text'],
+                        'active' => $active
+                    );
+    }
+
+    return $menu_arr;
+}
+
+function SRCH_getContentTypesAjax()
+{
+    global $_PLUGINS;
+
+    if ( !COM_isAjax()) die();
+
+    $contentTypes = array();
+    $retval = array();
+
+    $contentTypes[] = 'article';
+
+    foreach ($_PLUGINS as $pi_name) {
+        if (function_exists('plugin_getiteminfo_' . $pi_name)) {
+            $contentTypes[] = $pi_name;
+        }
+    }
+
+    $retval['errorCode'] = 0;
+    $retval['contenttypes'] = $contentTypes;
+
+    $retval['statusMessage'] = 'Initialization Successful';
+
+    $return["js"] = json_encode($retval);
+
+    echo json_encode($return);
+    exit;
+}
+
+
+function SRCH_getContentListAjax()
+{
+    global $_PLUGINS;
+
+    if ( !COM_isAjax()) die();
+
+    USES_searcher_class_indexer();
+
+    if ( !isset($_POST['type'])) die();
+
+    $type = COM_applyFilter($_POST['type']);
+
+    $contentList = array();
+    $retval = array();
+
+    $rc = PLG_getItemInfo($type,'*','id');
+    foreach ( $rc AS $id ) {
+        $contentList[] = $id;
+    }
+
+    \Searcher\Indexer::Removeall($type);
+
+    $retval['errorCode'] = 0;
+    $retval['contentlist'] = $contentList;
+    $retval['statusMessage'] = 'Content List Successful';
+
+    $return["js"] = json_encode($retval);
+
+    echo json_encode($return);
+    exit;
+}
+
+function SRCH_indexContentItemAjax()
+{
+    global $_PLUGINS;
+
+    if ( !COM_isAjax()) die();
+
+    if ( !isset($_POST['type'])) die();
+    if ( !isset($_POST['id'])) die();
+
+    USES_searcher_class_indexer();
+
+    $type = COM_applyFilter($_POST['type']);
+    $id   = COM_applyFilter($_POST['id']);
+
+    $contentList = array();
+    $retval = array();
+
+    $contentInfo = PLG_getItemInfo($type,$id,'id,date,title,searchidx,author,hits,perms');
+
+    if ( is_array($contentInfo) && count($contentInfo) > 0 ) {
+        $props = array(
+            'item_id' => $id,
+            'type'  => $type,
+            'title' => $contentInfo['title'],
+            'content' => $contentInfo['searchidx'],
+            'perms' => array(
+                'owner_id' => $contentInfo['perms']['owner_id'],
+                'group_id' => $contentInfo['perms']['group_id'],
+                'perm_owner' => $contentInfo['perms']['perm_owner'],
+                'perm_group' => $contentInfo['perms']['perm_group'],
+                'perm_members' => $contentInfo['perms']['perm_members'],
+                'perm_anon' => $contentInfo['perms']['perm_anon'],
+            ),
+        );
+
+        \Searcher\Indexer::IndexDoc($props);
+        if ( $type != 'forum' ) {
+            plugin_IndexAll_comments($type, $id, $props['perms']);
+        }
+
+        $retval['errorCode'] = 0;
+        $retval['statusMessage'] = 'Content Item Index Successful';
+    } else {
+        $retval['errorCode'] = -1;
+        $retval['statusMessage'] = 'Error indexing content';
+    }
+
+    $return["js"] = json_encode($retval);
+
+    echo json_encode($return);
+    exit;
+}
+// main driver
+
+$action = '';
+$expected = array('reindex','getcontenttypes','getcontentlist','index');
+foreach($expected as $provided) {
+    if (isset($_POST[$provided])) {
+        $action = $provided;
+    } elseif (isset($_GET[$provided])) {
+	    $action = $provided;
+    }
+}
+
+if ( isset($_POST['cancelbutton'])) COM_refresh($_CONF['site_admin_url'].'/plugins/searcher/index.php');
+
+switch ($action) {
+    case 'reindex':
+        $pagetitle = $LANG_SRCH['reindex_title'];
+        $page .= SRCHER_reindex();
+        break;
+    case 'getcontenttypes' :
+        // return json encoded list of content types
+        SRCH_getContentTypesAjax();
+        break;
+    case 'getcontentlist' :
+        // return list of all content type ids
+        SRCH_getContentListAjax();
+        break;
+    case 'index' :
+        // index a content item via ajax
+        SRCH_indexContentItemAjax();
+        break;
+
+    default :
+        $page = SRCHER_reindex();
+        break;
+}
+
+$display  = COM_siteHeader('menu', $LANG_SRCH_ADM['searcher_admin']);
+$display .= $page;
+$display .= COM_siteFooter();
+echo $display;
+?>
