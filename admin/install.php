607c590c1ea6f6d3384f5a9afebca25c2852be71
Lee Garner <lee@leegarner.com>
2018-12-15 08:59:22 -0800

    Just 404 if unauthorized for installation page

--- admin/install.php
+++ admin/install.php
@@ -1,44 +1,33 @@
 <?php
 /**
-*  Installation routine for the Searcher plugin for GLFusion
-*
-*   @author     Lee Garner <lee@leegarner.com>
-*   @copyright  Copyright (c) 2017 Lee Garner <lee@leegarner.com>
-*   @package    searcher
-*   @version    0.0.1
-*   @license    http://opensource.org/licenses/gpl-2.0.php
-*               GNU Public License v2 or later
-*   @filesource
-*/
-
-/**
- *  Include required glFusion common functions
+ * Installation routine for the Searcher plugin for GLFusion.
+ *
+ * @author      Lee Garner <lee@leegarner.com>
+ * @copyright   Copyright (c) 2017 Lee Garner <lee@leegarner.com>
+ * @package     searcher
+ * @version     0.1.2
+ * @license     http://opensource.org/licenses/gpl-2.0.php
+ *              GNU Public License v2 or later
+ * @filesource
  */
+
+/** Include required glFusion common functions. */
 require_once '../../../lib-common.php';
 
 // Only let Root users access this page
 if (!SEC_inGroup('Root')) {
     // Someone is trying to illegally access this page
     COM_errorLog("Someone has tried to illegally access the searcher install/uninstall page.  User id: {$_USER['uid']}, Username: {$_USER['username']}, IP: $REMOTE_ADDR",1);
-    $display = COM_siteHeader();
-    $display .= COM_startBlock($LANG_PHOTO['access_denied']);
-    $display .= $LANG_PHOTO['access_denied_msg'];
-    $display .= COM_endBlock();
-    $display .= COM_siteFooter(true);
-    echo $display;
+    COM_404();
     exit;
 }
 
-/**
-*  Include required plugin common functions
-*/
+/** Include required plugin common functions. */
 $base_path  = "{$_CONF['path']}plugins/searcher";
 require_once "$base_path/autoinstall.php";
 USES_lib_install();
 
-/*
-* Main Function
-*/
+// MAIN
 if (SEC_checkToken()) {
     if ($_GET['action'] == 'install') {
         if (plugin_install_searcher()) {
