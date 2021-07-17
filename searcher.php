64c96fcbde3617c01a427bfd3786b6bd4acaa47e
Lee Garner <lee@leegarner.com>
2021-07-17 13:43:35 -0700

    Bump version

--- searcher.php
+++ searcher.php
@@ -6,9 +6,9 @@
  * glFusion configuration system, such as allowed file types.
  *
  * @author      Lee Garner <lee@leegarner.com>
- * @copyright   Copyright (c) 2017-2020 Lee Garner <lee@leegarner.com>
+ * @copyright   Copyright (c) 2017-2021 Lee Garner <lee@leegarner.com>
  * @package     searcher
- * @version     v1.1.0
+ * @version     v1.1.1
  * @license     http://opensource.org/licenses/gpl-2.0.php
  *              GNU Public License v2 or later
  * @filesource
@@ -23,7 +23,7 @@ global $_SRCH_CONF;
 
 $_SRCH_CONF['pi_name']            = 'searcher';
 $_SRCH_CONF['pi_display_name']    = 'Searcher';
-$_SRCH_CONF['pi_version']         = '1.1.0';
+$_SRCH_CONF['pi_version']         = '1.1.1';
 $_SRCH_CONF['gl_version']         = '1.7.8';
 $_SRCH_CONF['pi_url']             = 'http://www.glfusion.org';
 
